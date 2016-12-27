60c75ce7f0fa5f4e658c48c9a51ce1398e54b71f
VijayalakshmiKumar02 <Vijayalakshmi.K4@cognizant.com>
2016-12-27 18:23:05 +0530

    Update build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -6,10 +6,11 @@ git clone concourse-java-maven-test-prj1 resource-app
 echo "creating file"
 cd resource-app
 echo "created the file"
-mvn clean compile
+mvn clean 
 echo " done compiling"
+mvn compile
 mvn install
 echo "done installing"
-#java de/bitkings/nitram509/ConcourseJavaMavenTestPrjApplication
+mvn  package exec:java de/bitkings/nitram509/ConcourseJavaMavenTestPrjApplication
 mvn package 
 echo "done running"
